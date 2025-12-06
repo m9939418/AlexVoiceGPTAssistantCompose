ba85ab1535b9aa543736bcf442b2dbf202a0f407
Alex <m9939418@gmail.com>
2025-12-06 22:42:54 +0800

    Add App Hit

---
+++ app/src/main/java/com/alex/yang/alexvoicegptassistantcompose/App.kt
@@ -0,0 +1,12 @@
+package com.alex.yang.alexvoicegptassistantcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AlexYang on 2025/12/6.
+ *
+ *
+ */
+@HiltAndroidApp
+class App : Application()
